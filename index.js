b1ac1ad1ef573d6a62270f64c1630a1a64220e69
Ezabulbary <ezabul.bary@gmail.com>
2022-05-29 01:59:08 +0600

    varify admin function added

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const cors = require('cors');
-const app = express()
+const app = express();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sqbew.mongodb.net/?retryWrites=true&w=majority`;
@@ -18,8 +35,34 @@ async function run() {
         const toolsCollection = client.db('jan-tik').collection('tools');
         const reviewsCollection = client.db('jan-tik').collection('reviews');
         const orderCollection = client.db('jan-tik').collection('orders');
+        const userCollection = client.db('jan-tik').collection('user');
 
         console.log('db connected');
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next()
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+        };
+
+        // user
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ result, token });
+        })
 
         // tools
         app.get('/tools', async (req, res) => {
@@ -56,8 +99,15 @@ async function run() {
 
         // orders
         app.post('/orders', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
+        });
+
+        app.get('/orders', async (req, res) => {
+            const email = req.query.customerEmail;
+            const query = { customerEmail: email };
+            const result = await orderCollection.find(query).toArray();
             res.send(result);
         });
 
