13355bfd88b2ae964a937babb076cdebb975b941
Ezabulbary <ezabul.bary@gmail.com>
2022-05-29 11:46:53 +0600

    verify admin

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ async function run() {
         const toolsCollection = client.db('jan-tik').collection('tools');
         const reviewsCollection = client.db('jan-tik').collection('reviews');
         const orderCollection = client.db('jan-tik').collection('orders');
-        const userCollection = client.db('jan-tik').collection('user');
+        const usersCollection = client.db('jan-tik').collection('users');
         const profileCollection = client.db('jan-tik').collection('profile');
 
         console.log('db connected');
 
         const verifyAdmin = async (req, res, next) => {
             const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester });
+            const requesterAccount = await usersCollection.findOne({ email: requester });
             if (requesterAccount.role === 'admin') {
                 next();
             }
@@ -60,93 +60,122 @@ async function run() {
             const updateDoc = {
                 $set: user,
             };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
             res.send({ result, token });
         });
 
-        // tools
-        app.get('/tools', async (req, res) => {
-            const tools = await toolsCollection.find().toArray();
-            res.send(tools);
+        app.get('/user', async (req, res) => {
+            const users = await usersCollection.find().toArray();
+            res.send(users);
         });
 
-        app.get('/tools/:id', async (req, res) => {
-            const id = req.params.id;
-            const tool = await toolsCollection.findOne({ _id: ObjectId(id) });
-            res.send(tool);
-        });
-
-        app.put('/tools/:id', async (req, res) => {
-            const id = req.params.id;
-            const updateTool = req.body;
-            const filter = { _id: ObjectId(id) };
-            const options = { upset: true };
-            const updateDoc = {
-                $set: {
-                    available_quantity: updateTool.newQuantity
-                }
-            };
-            const result = await toolsCollection.updateOne(filter, updateDoc, options);
-            const item = await toolsCollection.findOne(filter);
-            res.send({ result, item });
-        });
-
-        // reviews
-        app.get('/reviews', async (req, res) => {
-            const reviews = await reviewsCollection.find().toArray();
-            res.send(reviews);
-        });
-
-        app.post('/addreview', async (req, res) => {
-            const newReview = req.body;
-            const result = await reviewsCollection.insertOne(newReview);
-            res.send(result);
-        });
-
-        // orders
-        app.post('/orders', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
-            res.send(result);
-        });
-
-        app.get('/orders', verifyJWT, async (req, res) => {
-            const email = req.query.customerEmail;
-            const decodedEmail = req.decoded.email;
-            if (email === decodedEmail){
-                const query = { customerEmail: email };
-                const result = await orderCollection.find(query).toArray();
-                res.send(result);
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await usersCollection.updateOne(filter, updateDoc)
+                res.send(result)
             }
             else {
-                return res.status(403).send({ message: 'forbidden access' });
+                res.status(403).send({ message: 'forbidden access' })
             }
         });
 
-        app.delete('/orders/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await orderCollection.deleteOne(query);
-            res.send(result);
-        });
-
-        // profile
-        app.put('/myprofile/:email', async (req, res) => {
+        app.get('/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const info = req.body;
-            const filter = { email: email };
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: info,
-            };
-            const result = await profileCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
-        });
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
 
-    } finally {
-        // await client.close();
-    }
+    // tools
+    app.get('/tools', async (req, res) => {
+        const tools = await toolsCollection.find().toArray();
+        res.send(tools);
+    });
+
+    app.get('/tools/:id', async (req, res) => {
+        const id = req.params.id;
+        const tool = await toolsCollection.findOne({ _id: ObjectId(id) });
+        res.send(tool);
+    });
+
+    app.put('/tools/:id', async (req, res) => {
+        const id = req.params.id;
+        const updateTool = req.body;
+        const filter = { _id: ObjectId(id) };
+        const options = { upset: true };
+        const updateDoc = {
+            $set: {
+                available_quantity: updateTool.newQuantity
+            }
+        };
+        const result = await toolsCollection.updateOne(filter, updateDoc, options);
+        const item = await toolsCollection.findOne(filter);
+        res.send({ result, item });
+    });
+
+    // reviews
+    app.get('/reviews', async (req, res) => {
+        const reviews = await reviewsCollection.find().toArray();
+        res.send(reviews);
+    });
+
+    app.post('/addreview', async (req, res) => {
+        const newReview = req.body;
+        const result = await reviewsCollection.insertOne(newReview);
+        res.send(result);
+    });
+
+    // orders
+    app.post('/orders', async (req, res) => {
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.send(result);
+    });
+
+    app.get('/orders', verifyJWT, async (req, res) => {
+        const email = req.query.customerEmail;
+        const decodedEmail = req.decoded.email;
+        if (email === decodedEmail) {
+            const query = { customerEmail: email };
+            const result = await orderCollection.find(query).toArray();
+            res.send(result);
+        }
+        else {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+    });
+
+    app.delete('/orders/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await orderCollection.deleteOne(query);
+        res.send(result);
+    });
+
+    // profile
+    app.put('/myprofile/:email', async (req, res) => {
+        const email = req.params.email;
+        const info = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: info,
+        };
+        const result = await profileCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+    });
+
+} finally {
+    // await client.close();
+}
 }
 run().catch(console.dir);
 
