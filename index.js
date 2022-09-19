fd1f18b64f35949eb70829c7edeff7750b4b020c
Ezabulbary <ezabul.bary@gmail.com>
2022-09-19 23:15:25 +0600

    stripe api added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 var nodemailer = require('nodemailer');
 const mg = require('nodemailer-mailgun-transport');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json())
@@ -90,6 +91,18 @@ async function run() {
                 return res.status(403).send({ message: 'Forbidden access' });
             }
         };
+
+        app.post('/create-payment-intent', verifyJWT, async(req, res) => {
+            const tool = req.body;
+            const pricePerUnit = tool.pricePerUnit;
+            const amount = pricePerUnit*100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount : amount,
+                currency : 'usd',
+                payment_method_type : ['card']
+            });
+            res.send({clientSecret: paymentIntent.client_secret})
+        });
 
         // user
         app.put('/user/:email', async (req, res) => {
